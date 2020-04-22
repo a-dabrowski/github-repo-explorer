30d37160e348a302445b14260e5971c21a41c43f
Adam Dąbrowski <adam.dabrowski@publicis.com>
2020-04-22 20:48:49 +0200

    Query for find searches usernames

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_URL = 'https://api.github.com'
 
 export const findUser = (query: string) => {
-  return axios.get(`${API_URL}/users?q=${query}`).then(response => {
+  return axios.get(`${API_URL}/search/users`, {params: {q: query + ' in:login'}}).then(response => {
     return response;
   });
 };
