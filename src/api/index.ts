06eb064ed33e62f57e76e036db77e86cc3ba2aeb
Adam Dąbrowski <adam.dabrowski@publicis.com>
2020-04-23 14:41:20 +0200

    Method api for fetching repos

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
 const API_URL = 'https://api.github.com'
 
+export interface iFetchedUser {
+  login: string,
+  repos_url: string,
+  id: number
+}
+
 export const findUser = (query: string) => {
   return axios.get(`${API_URL}/search/users`, {params: {q: query + ' in:login'}}).then(response => {
+    return response;
+  });
+};
+
+export const fetchUserRepos = (userLogin: string) => {
+  return axios.get(userLogin).then(response => {
     return response;
   });
 };
