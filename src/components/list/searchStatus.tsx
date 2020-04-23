735e4c63ec55ee17b84484702af5e5312870cb28
Adam Dąbrowski <adam.dabrowski@publicis.com>
2020-04-23 18:55:21 +0200

    Loading bar

--- src/components/list/searchStatus.tsx
+++ src/components/list/searchStatus.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
 import { useSelector } from "react-redux";
 import {
-  selectIsResolved,
   selectIsError,
+  selectIsLoading,
+  selectIsResolved,
   selectSearchQuery
 } from "./listSlice";
+
+const PROGRESS_SIZE = 60;
 
 const useStyles = makeStyles(() =>
   createStyles({
     statusWrapper: {
+      position: 'relative',
       margin: '12px 0'
+    },
+    loadingCircle: {
+      position: 'relative',
+      left: `calc(50% - ${PROGRESS_SIZE / 2}px)`
     },
     statusError: {
       color: 'red'
@@ -20,13 +29,17 @@ const useStyles = makeStyles(() =>
 );
 
 export function SearchStatus() {
-  const searchQuery = useSelector(selectSearchQuery);
-  const isSearchResolved = useSelector(selectIsResolved);
-  const isSearchError = useSelector(selectIsError);
   const classes = useStyles();
+  const isSearchError = useSelector(selectIsError);
+  const isSearchLoading = useSelector(selectIsLoading);
+  const isSearchResolved = useSelector(selectIsResolved);
+  const searchQuery = useSelector(selectSearchQuery);
 
   return (
     <div className={classes.statusWrapper}>
+      {isSearchLoading && (
+      <CircularProgress className={classes.loadingCircle} size={PROGRESS_SIZE} />
+      )}
       {isSearchResolved && (
         <Typography variant="subtitle1">Showing users for "{searchQuery}"</Typography>
       )}
